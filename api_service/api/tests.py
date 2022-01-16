da0a71ad0130396d74a1dc9cb24beff98094ae72
Juan Lopez <juan.lopez@mercadolibre.cl>
2022-01-16 19:51:32 -0300

    example test

--- api_service/api/tests.py
+++ api_service/api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from rest_framework import status
 
-# Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('/stock')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_index_fail(self):
+        """The index page loads properly"""
+        response = self.client.get('/fake')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
